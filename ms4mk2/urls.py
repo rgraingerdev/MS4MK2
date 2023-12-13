ff6a3eda3b3c8ad5618ad21a7478794a1df57244
rgraingerdev <121545703+rgraingerdev@users.noreply.github.com>
2023-12-13 22:44:15 +0000

    corrected var declaration for urlpatterns

--- ms4mk2/urls.py
+++ ms4mk2/urls.py
@@ -18,9 +18,10 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('store.urls'))
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
