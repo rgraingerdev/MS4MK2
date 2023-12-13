deb027c505ee5094ea572310410b4f64edb6cc6b
rgraingerdev <121545703+rgraingerdev@users.noreply.github.com>
2023-12-13 22:21:33 +0000

    TS heroku deploy

--- ms4mk2/settings.py
+++ ms4mk2/settings.py
@@ -120,7 +120,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS= [
-    os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    '/var/www/static/',
 ]
 
